4b0cb49e890f3052cb02b1e4d889a5fe595cd6f7
gardad20 <gardad20@wfu.edu>
2022-03-15 15:41:57 -0400

    New zip file first commit

---
+++ src/MultiMerge.java
@@ -0,0 +1,13 @@
+/*
+You should have your Multiway Merge methods here
+ */
+
+//do everything here
+//1) Make min heap (each node is a record)
+//2) Parse and add to min heap
+//3) Sort
+// merge by comparing first of each block (use min heap to get first in block)
+//length is one 8-way merge (DSA 14.2)
+
+public class MultiMerge {
+}
