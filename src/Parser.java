64fcf226dd3b848adfd26540b0b36f94e035ce87
SophieLee <leesb19@wfu.edu>
2022-03-15 16:32:15 -0400

    started parsing

--- src/Parser.java
+++ src/Parser.java
@@ -29,6 +29,21 @@ public class Parser {
         which enlarge the size of file
          */
         RandomAccessFile ifrd = new RandomAccessFile(infoToParse, "r");
+
+        //need to wrap this in some sort of loop
+        int position = ifrd.readInt();
+        int length = ifrd.readInt();
+
+        byte[] inputBuff = new byte[8192]; // "a block is 8,192 bytes" according to project description
+
+        raf.readFully(inputBuff, position, length); //use position and length to add first block to input buffer
+
+        //now: add this block to RAM minheap
+
+        //then: use seek to start getting next block from Run file
+        long currFilePointer = position + length; //would this need to be manually converted to long?
+        raf.seek(currFilePointer);
+
     }
     
     //you can process the files you read in here and call your mutiway-
