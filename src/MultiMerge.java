f32ca8f46fe0cbce20b6edfd6118dd451e6e37af
gardad20 <gardad20@wfu.edu>
2022-03-19 17:17:22 -0400

    03/19

--- src/MultiMerge.java
+++ src/MultiMerge.java
@@ -8,7 +8,8 @@ You should have your Multiway Merge methods here
 //3) Sort
 // merge by comparing first of each block (use min heap to get first in block)
 //length is one 8-way merge (DSA 14.2)
-
+import java.util.ArrayList;
+import java.util.Arrays;
 public class MultiMerge {
 
     /*
@@ -16,6 +17,13 @@ public class MultiMerge {
         compare first record of each arrayList, see which is smaller, then add the smaller to the output buffer
             then, the output buffer is written to another file
      */
+    public MultiMerge(){}
 
+    //Takes an array of Arraylists and merges them (Not sure if correct)
+    public static void merge(ArrayList<Record>[] toSort){
+        for(int i=0; i< toSort.length; i++){
+            Arrays.sort(toSort[i].toArray());
+        }
+    }
 
 }
