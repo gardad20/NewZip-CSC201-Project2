df5374a98aefd12ad1b06bb26da94e991106304b
gardad20 <gardad20@wfu.edu>
2022-03-19 17:15:37 -0400

    03/19

--- src/Parser.java
+++ src/Parser.java
@@ -39,26 +39,26 @@ public class Parser {
         ArrayList<MergeInfo> infoList = new ArrayList<MergeInfo>();
 
         // read pos/len info into a Merge object, then into infoList arrayList
-        for(int i=0; i<ifrd.length(); i++){
+        for(int i=0; i<ifrd.length()/8; i++){
             position = ifrd.readInt();
             length = ifrd.readInt();
             MergeInfo merge = new MergeInfo(position, length);
             infoList.add(merge);
         }
 
-        ArrayList<Record> inBuff = new ArrayList<Record>(512); // buffer to store Blocks as they go into ListArray
+        ArrayList<Record> inBuff = new ArrayList<Record>(); // buffer to store Blocks as they go into ListArray
         ArrayList<ArrayList<Record>> listArray = new ArrayList<ArrayList<Record>>(); // to store all Block ArrayLists
 
-        for (int j = 0; j < raf.length()/8; j++){
-            byte[] inputBuff = new byte[8192]; // declare input buffer
+        for (int j = 0; j < ifrd.length()/8; j++){
             int currPosition = infoList.get(j).getStart();
             int currLength = infoList.get(j).getLength();
+            byte[] inputBuff = new byte[currLength*8]; // declare input buffer
 
             // add a block to input buffer (using pos and len)
             raf.readFully(inputBuff, currPosition, currLength);
 
             // divide inputBuff into records, then put those records into inBuff arrayList
-            for (int k = 0; k < 512; k++){
+            for (int k = 0; k < 512*8; k++){
                 byte[] recordX = java.util.Arrays.copyOfRange(inputBuff, k, k+16); // isolates one record from inputBuff
                 Record myRec = new Record(recordX);
                 inBuff.add(myRec);
@@ -71,18 +71,12 @@ public class Parser {
             // add inBuff contents to listArray --> just add it now, multiway merge later
             listArray.add(inBuff);
 
-
             // clear inBuff in prep for next iteration
             inBuff.clear();
 
             //use seek to (reset file pointer position) & start getting next block from Run file
             long currFilePointer = currPosition + currLength; //would this need to be manually converted to long?
             raf.seek(currFilePointer);
-        }
-
-        // What is this doing ??
-        for(int i=0; i<inBuff.size(); i++){
-            System.out.println(inBuff.get(i).toString());
         }
 
 
